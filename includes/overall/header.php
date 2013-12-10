321999445421077dc861f4c330d3e1d455aa4bc2
Navneet Pandey <npandey057@gmail.com>
2013-12-10 21:00:43 +0530

    Remove Spaces

--- includes/overall/header.php
+++ includes/overall/header.php
@@ -1,4 +1,2 @@
-	<link href="css/main.css" rel="stylesheet" type="text/css" media="screen">
-
-
-            <?php include "includes/aside.php"; ?>
+<link href="css/main.css" rel="stylesheet" type="text/css" media="screen">
+<?php include "includes/aside.php"; ?>
